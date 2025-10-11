aceb129b0229a439c1d68d6bf8a708b7e3614397
nadiabenslt <166945599+nadiabenslt@users.noreply.github.com>
2025-10-11 15:41:26 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,6 +12,7 @@ export default function Home() {
               src={img}
               alt="developer illustration"
               className="hero-img img-fluid"
+              style={{"with": "100px"}}
             />
           </div>
           <div className="col-lg-6 col-md-12 mb-4 mb-lg-0">
